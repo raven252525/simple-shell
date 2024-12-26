375e62618e57d49e5445a225c4ad9dcab6288420
raven252525 <maximyurkovsky@gmail.com>
2024-12-25 19:58:19 -0800

    [5]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -15,8 +16,16 @@ int main() {
     
     if(input == "exit 0"){
       break;
+    }// checks for exit condition 0
+
+    std::string echoCheck = input.substr(0,4);
+    if(echoCheck == "echo"){
+      int length = input.length() - echoCheck.length(); 
+      std::string echoPrint = input.substr(6, length);//skip the 5th character bc we are skipping the whitespace
+      std::cout << echoPrint << std::endl;
     }
 
+    
     std::cout << input << ": command not found\n";
   };
 }
