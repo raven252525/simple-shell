691dd78da99d13b69898ed39064fe3321c12cc30
raven252525 <maximyurkovsky@gmail.com>
2024-12-26 14:54:21 -0800

    7

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+
+std::string getPath(std::string command){
+  std::string envPath = std::getenv("PATH");
+
+  std::stringstream ss(envPath); // add path environment as a string stream obj
+  st::string path;// where to place each of the directories separated by the ; in path before adding to return str
+
+  while(!ss.eof()){
+    std::getline(ss, path, ;) // windows uses ; as separator for directories in PATH
+
+    std::string absPath = path + '/' + command;
+
+    if(filesystem::exists(absPath)){
+      return absPath;
+    }
+  }
+
+  return "";//returns an empty string if there is no path that exists
+}
+
+
 
 int main(){
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // Uncomment this block to pass the first stage
 
   while(true){
     std::cout << "$ ";
@@ -21,20 +41,25 @@ int main(){
     std::string cmdCheck = input.substr(0,4);
     std::string restOfInput = input.substr(5);
 
-    
     if(cmdCheck == "echo"){
       std::cout << restOfInput << std::endl;
-    }
+    }//echo
     else if(cmdCheck == "type"){
       if(restOfInput.find("type") == 0 || restOfInput.find("echo") == 0 || restOfInput.find("exit") == 0){
         std::cout << restOfInput << " is a shell builtin\n"; 
       }
       else{
-        std::cout << restOfInput << ": not found\n";
+        std::string path = getPath(restOfInput); // returns the path of the input to string
+        if(path.empty()){
+          std::cout << restOfInput << ": not found\n";
+        }
+        else{
+          std::cout << restOfInput << " is " << path << std::endl;
+        }//path in type
       }
-    }
+    }//type
     else{
       std::cout << input << ": command not found\n";
-    }
+    }//invalid
   }
 }
