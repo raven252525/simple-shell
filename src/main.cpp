b890bdc42d139d02bfb1cc95cc10297449477849
raven252525 <maximyurkovsky@gmail.com>
2024-12-25 20:01:23 -0800

    [5]

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,8 @@ int main() {
 
     std::string echoCheck = input.substr(0,4);
     if(echoCheck == "echo"){
-      int length = input.length() - echoCheck.length(); 
-      std::string echoPrint = input.substr(6, length);
+      int length = input.length() - echoCheck.length() + 1; 
+      std::string echoPrint = input.substr(5, length);
       std::cout << echoPrint << std::endl;
     }
 
