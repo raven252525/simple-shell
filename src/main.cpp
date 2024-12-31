e56b1ba87fe610eb909c57d789bc1fa4f2e6dfe4
raven252525 <maximyurkovsky@gmail.com>
2024-12-30 23:21:47 -0800

    single quotes echo

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ void executeChild(std::string pathCh, std::vector<std::string> args){
 int checkCharFrequency(std::string s, const char* toCheck){
   int count;
   for(int i = 0; i < s.size(); i++){
-    if(s[i] == toCheck){
+    if(s[i] == toCheck*){
       count++;
     } 
   }
